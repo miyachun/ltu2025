0a95761e05e1f022ed150c95560e5e96e428c647
miyachun <100542608+miyachun@users.noreply.github.com>
2025-06-18 16:39:02 +0800

    Update a01.py

--- yolo/a01.py
+++ yolo/a01.py
@@ -6,7 +6,4 @@ model = YOLO("path/to/best.pt")  # load a custom model
 
 # Validate the model
 metrics = model.val()  # no arguments needed, dataset and settings remembered
-metrics.box.map  # map50-95
-metrics.box.map50  # map50
-metrics.box.map75  # map75
-metrics.box.maps  # a list contains map50-95 of each category
+metrics.box.map
