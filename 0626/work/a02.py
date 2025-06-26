15eadc50becfa02a2c0608bb37215717ba890df3
miyachun <100542608+miyachun@users.noreply.github.com>
2025-06-26 11:13:29 +0800

    Add files via upload

---
+++ 0626/work/a02.py
@@ -0,0 +1,28 @@
+#https://opendata.cwa.gov.tw/dist/opendata-swagger.html
+import json,urllib.request
+
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+API_KEY='XXXX'
+
+url='https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-073?Authorization=CWB-F6A0BAC9-E773-4B09-B048-D30A55EEBED2&ElementName=%E6%BA%AB%E5%BA%A6'
+
+
+data = urllib.request.urlopen(url).read()
+output = json.loads(data)
+location=output['records']['Locations'][0]['Location']
+
+'''
+for i in location:
+    print(f'{i}')
+'''
+
+
+for i in location:
+    city = i['LocationName']    # 台中市
+    Temp = i['WeatherElement'][0]['Time'][0]['ElementValue'][0]['Temperature']
+    
+    print(f'{city}未來 3 天溫度{Temp}')
