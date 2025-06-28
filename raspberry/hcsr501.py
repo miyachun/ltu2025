28f83e9c302cdfb6a07b5339051b8d7d6b82370b
miyachun <100542608+miyachun@users.noreply.github.com>
2025-06-28 12:28:29 +0800

    Update hcsr501.py

--- raspberry/hcsr501.py
+++ raspberry/hcsr501.py
@@ -1,25 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-
+ 
 GPIO.setmode(GPIO.BCM)
-
-PIR_PIN = 21
-CNT = 0
-
+GPIO.setwarnings(False)
+PIR_PIN = 23
 GPIO.setup(PIR_PIN, GPIO.IN)
 
-def MOTION(PIR_PIN):
-  print ("Motion Detected")
+print('Starting up the PIR Module (click on STOP to exit)')
+time.sleep(1)
+print ('Ready')
 
-print ("PIR Module Test (CTRL+C to exit)")
-time.sleep(2)
-print ("Ready")
-
-
-try:
-	GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
-	while 1:
-		time.sleep(100)
-except KeyboardInterrupt:
-	print ("Quit")
-	GPIO.cleanup()
+while True:
+  if GPIO.input(PIR_PIN):
+    print('Motion Detected')
+  time.sleep(1)
