adf464d400497c849ce2d994666284c9e1100c55
miyachun <100542608+miyachun@users.noreply.github.com>
2025-06-28 12:10:49 +0800

    Update hcsr04.py

--- raspberry/hcsr04.py
+++ raspberry/hcsr04.py
@@ -1,29 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
+
 GPIO.setmode(GPIO.BCM)
+
 
 TRIG = 23  # Trig GPIO 23
 ECHO = 24  # Echo GPIO 24
+
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 def distance():
-
+    
     GPIO.output(TRIG, True)
 
+    
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
 
+    
     while GPIO.input(ECHO) == 0:
         start_time = time.time()
 
+    
     while GPIO.input(ECHO) == 1:
         stop_time = time.time()
 
+    
     time_elapsed = stop_time - start_time
 
+    
     distance = (time_elapsed * 34300) / 2
 
     return distance
@@ -33,6 +41,7 @@ try:
         dist = distance()
         print(f"Measured Distance = {dist:.2f} cm")
         time.sleep(1)
+
 
 except KeyboardInterrupt:
     print("Measurement stopped by User")
