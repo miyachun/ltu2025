14530a659bcf6f709b56a47026cc02c11c136352
miyachun <100542608+miyachun@users.noreply.github.com>
2025-07-03 14:50:19 +0800

    Create a09.py

---
+++ 0703/a09.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+
+line_bot_api = LineBotApi('LINE_CHANNEL_ACCESS_TOKEN')
+line_handler = WebhookHandler('LINE_CHANNEL_SECRET')
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+line_bot_api.push_message('User ID', TextSendMessage(text='Hello World!!!'))
