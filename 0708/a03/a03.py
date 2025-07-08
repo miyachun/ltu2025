76f6a1762cf52d3c9013d8997bedd4c06b0cc75e
miyachun <100542608+miyachun@users.noreply.github.com>
2025-07-08 13:38:30 +0800

    Add files via upload

---
+++ 0708/a03/a03.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+df = pd.read_csv('static/people108.csv')
+
+@app.route('/')
+def index():
+    dataName=df.iloc[:,1].tolist()
+    data=df.iloc[:,4].tolist()
+    return render_template("myindex02.html", dataName=dataName,data=data)
+  
+
+if __name__ == "__main__":
+    app.run(debug=True)
